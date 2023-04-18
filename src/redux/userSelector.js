dc8578d3af23918ed7684336026e4cf908e3cc02
Lukast2008 <Lukast2008@users.noreply.github.com>
2023-04-18 13:42:53 +0000

    deploy: fdc6b96270bf7957d1a02d955c051a12cfa12025

--- src/redux/userSelector.js
+++
@@ -1,5 +0,0 @@
-export const userSelector = (state) => state.users.item;
-
-export const selectLoading = (state) => state.users.loading;
-
-export const filterTweetSelector = state => state.users.filter
